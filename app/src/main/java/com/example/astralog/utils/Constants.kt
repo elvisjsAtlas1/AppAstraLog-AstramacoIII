b39da6c824a315a76411b582113354522f082f75
elvisjsAtlas1 <chat02jselvis@gmail.com>
2026-06-26 00:55:36 -0500

    app AstraLog

--- app/src/main/java/com/example/astralog/utils/Constants.kt
+++ app/src/main/java/com/example/astralog/utils/Constants.kt
@@ -1,6 +1,7 @@
 package com.example.astralog.utils
 
 object Constants {
-    const val BASE_URL = "http://192.168.0.102:8080/api/"
+    //const val BASE_URL = "http://192.168.0.102:8080/api/"
+    const val BASE_URL = "https://lucky-endurance-production.up.railway.app/api/"
     //cambiar la direccion IP de la red a donde esta apuntando la pc y el celular
 }
